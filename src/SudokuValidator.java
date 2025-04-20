72cf49a23b50f08b0a2671bb3c04153a992c2efd
yaniomar2005 <yaniomar2005@gmail.com>
2025-04-20 14:40:33 +0300

    11threads

--- src/SudokuValidator.java
+++ src/SudokuValidator.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class SudokuValidator {
     private static int n;
     public static int[][] sudokuBoard;
+    int m = (int) Math.sqrt(n);
     private static ArrayList<Boolean> resultsForTheCurrentThreadsApproach;
     public static boolean[] allApproachesValidation = new boolean[3];
     public SudokuValidator(int[][] sudokuBoard) {
@@ -12,14 +13,31 @@ public class SudokuValidator {
     }
 
     // checkValidCol
-    static class ColumnValidator implements Runnable{
-
-        public ColumnValidator(){
+    static class ColumnValidator11Threads implements Runnable{
+        public ColumnValidator11Threads(){
         }
         public void run(){
-            // call the checkValidCol
+            boolean result;
+            for(int i=0; i<n; i++){
+                result =result && checkValidCol(i);
+            }
+            resultsForTheCurrentThreadsApproach.add(result);
         }
     }
+
+    static class RowValidator11Threads implements Runnable{
+        public RowValidator11Threads(){
+        }
+        public void run(){
+            boolean result;
+            for(int i=0; i<n; i++){
+                result =result && checkValidRow(i);
+            }
+            resultsForTheCurrentThreadsApproach.add(result);
+        }
+    }
+
+
     public static boolean checkValidCol(int colNumber){
         boolean [] seen = new boolean[n];
         for(int i = 0;i < n;i++){
@@ -70,14 +88,37 @@ public class SudokuValidator {
         allApproachesValidation[0] = result;
     }
 
+
     public static void checkValidSudokuMultithreadingApproachUsing11Threads(){
         resultsForTheCurrentThreadsApproach.clear();
-
+        long startTime = System.nanoTime();
+        Thread rowThread = new Thread(RowValidator11Threads());
+        Thread columnThread = new Thread(ColumnValidator11Threads());
+        Thread gridThreads[n];
+        rowThread.start();
+        columnThread.start();
+        int index=0;
+        for (int row = 0; row < n; row += m) {
+            for (int col = 0; col < n; col += m) {
+                gridThreads[index] = new Thread(new GridValidator(row, col));
+                gridThreads[index].start();
+                index++;
+            }
+        }
+        rowThread.join();
+        columnThread.join();
+        for(int i=0; i<index; i++){
+            gridThreads[i].join();
+        }
+        long endTime = System.nanoTime();
         boolean flag = true;
         for(boolean iterator : resultsForTheCurrentThreadsApproach){
             flag = flag && iterator;
         }
         allApproachesValidation[1] = flag;
+        long totalTime = startTime-endTime;
+        System.out.println("Total Time: "+totalTime);
+        System.out.println("Is Valid: "+flag);
     }
 
 
